5a0869377e26283fa80f5a3649478df643c15fc4
Kay <info@beckmann-md.de>
2024-01-28 12:23:39 +0100

    Show data from DB in 'Warenkorb'

--- shop/shop/views.py
+++ shop/shop/views.py
@@ -8,7 +8,15 @@ def shop(request):
   return render(request, 'shop/shop.html', ctx)
 
 def warenkorb(request):
-  return render(request, 'shop/warenkorb.html')
+  if request.user.is_authenticated:
+    kunde = request.user.kunde
+    bestellung, created = Bestellung.objects.get_or_create(kunde = kunde, erledigt = False)
+    artikels = bestellung.bestellteartikell_set.all()
+  else:
+    artikels = []
+    
+  ctx = {"artikels": artikels}
+  return render(request, 'shop/warenkorb.html', ctx)
 
 def kasse(request):
   return render(request, 'shop/kasse.html')
