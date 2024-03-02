2bc231989c204d64190764a378628b647e2e8ca8
Kay <info@beckmann-md.de>
2024-03-02 17:50:02 +0100

    error

--- shop/shop/views.py
+++ shop/shop/views.py
@@ -2,110 +2,107 @@ from django.shortcuts import render, redirect
 from django.urls import reverse
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-from . models import *
-from django.http import JsonResponse, HttpResponse
-import json
+from django.http import JsonResponse
 from django.contrib.auth import authenticate, login, logout
-from . forms import EigeneUserCreationForm
+from django.contrib.auth.forms import UserCreationForm
+from django.utils.safestring import mark_safe
+import json
+from . models import *
 import uuid
 from paypal.standard.forms import PayPalPaymentsForm
 
 def shop(request):
-    artikels = Artikel.objects.all()
-    ctx = {'artikels':artikels}
-    return render(request, 'shop/shop.html', ctx)
+  # ctx steht für CONTEXT
+  artikels = Artikel.objects.all()
+  ctx = {'artikels':artikels}
+  return render(request, 'shop/shop.html', ctx)
+
+def berechnungBestellung(request):
+  if request.user.is_authenticated:
+    kunde = request.user.kunde
+    bestellung, created = Bestellung.objects.get_or_create(kunde = kunde, erledigt = False)
+    artikels = bestellung.bestellteartikell_set.all()
+  else:
+    artikels = []
+    bestellung = []
+    
+  ctx = {"artikels": artikels, "bestellung":bestellung}
+  return ctx
 
 def warenkorb(request):
-    if request.user.is_authenticated:
-        kunde = request.user.kunde
-        bestellung, created = Bestellung.objects.get_or_create(kunde=kunde, erledigt=False)
-        artikels = bestellung.bestellteartikel_set.all()
-    else:
-        cookieDaten = gastCookie(request)
-        artikels = cookieDaten['artikels']
-        bestellung = cookieDaten['bestellung']
-
-    ctx = {"artikels":artikels, "bestellung":bestellung}        
-    return render(request, 'shop/warenkorb.html',ctx)
+  ctx = berechnungBestellung(request)
+  return render(request, 'shop/warenkorb.html', ctx)
 
 def kasse(request):
-    if request.user.is_authenticated:
-        kunde = request.user.kunde
-        bestellung, created = Bestellung.objects.get_or_create(kunde=kunde, erledigt=False)
-        artikels = bestellung.bestellteartikel_set.all()
-    else:
-        cookieDaten = gastCookie(request)
-        artikels = cookieDaten['artikels']
-        bestellung = cookieDaten['bestellung']
+  ctx = berechnungBestellung(request)
+  return render(request, 'shop/kasse.html', ctx)
 
-    ctx = {"artikels":artikels, "bestellung":bestellung}      
-    return render(request, 'shop/kasse.html', ctx)
-
-def artikelBackend(request):
-    daten = json.loads(request.body)
-    artikelID = daten['artikelID']
-    action = daten['action']
-    kunde = request.user.kunde
-    artikel = Artikel.objects.get(id=artikelID)
-    bestellung, created = Bestellung.objects.get_or_create(kunde=kunde, erledigt=False)
-    bestellteArtikel, created = BestellteArtikel.objects.get_or_create(bestellung=bestellung, artikel=artikel)
-
-    if action == 'bestellen':
-        bestellteArtikel.menge = (bestellteArtikel.menge +1)
-        messages.success(request, "Artikel wurde zum Warenkorb hinzugefügt.")
-    elif action == 'entfernen':
-        bestellteArtikel.menge = (bestellteArtikel.menge -1)
-        messages.warning(request, "Artikel wurde aus dem Warenkorb entfernt.")    
-
-    bestellteArtikel.save()
-
-    if bestellteArtikel.menge <= 0:
-        bestellteArtikel.delete()
-    
-    return JsonResponse("Artikel hinzugefügt", safe=False)
+def artikelbackend(request):
+  daten = json.loads(request.body)
+  artikelId = daten['artikelId']
+  action = daten['action']
+  kunde = request.user.kunde
+  artikel = Artikel.objects.get(id=artikelId)
+  bestellung, created = Bestellung.objects.get_or_create(kunde = kunde, erledigt=False)
+  bestellteArtikell, created = BestellteArtikell.objects.get_or_create(bestellung=bestellung, artikel=artikel)
+  
+  if action == 'bestellen':
+    bestellteArtikell.menge = bestellteArtikell.menge + 1
+    messages.success(request, "Artikel wurde zum Warenkorb hinzugefügt.")
+  elif action == 'entfernen':
+    bestellteArtikell.menge = bestellteArtikell.menge - 1
+    messages.warning(request, "Artikel wurde aus dem Warenkorb entfernt.")
+      
+  bestellteArtikell.save()
+  
+  if bestellteArtikell.menge <= 0:
+    bestellteArtikell.delete()
+  
+  return JsonResponse("Artikel hinzugefuegt", safe=False)
 
 def loginSeite(request):
-    seite = 'login'
-    if request.method == 'POST':
-        benutzername = request.POST['benutzername']
-        passwort = request.POST['passwort']
+  if request.method == 'POST':
+    benutzername = request.POST['benutzername']
+    passwort = request.POST['passwort']
+    
+    benutzer = authenticate(request, username=benutzername, password=passwort)
+    
+    if benutzer is not None:
+      login(request, benutzer)
+      return redirect('shop')
+    else:
+      messages.error(request, "Fehler beim Login.")
+      
+  seite = "login"
+  ctx = {"seite":seite}
+  return render(request, 'shop/login.html', ctx)
 
-        benutzer = authenticate(request, username=benutzername, password=passwort)
+def logoutSeite(request):
+  logout(request)
+  return redirect('shop')
 
-        if benutzer is not None:
-            login(request, benutzer)
-            return redirect('shop')
-        else:
-            messages.error(request, "Benutzername oder Passwort nicht korrekt.")
-
-    return render(request, 'shop/login.html', {'seite': seite})
-
-def logoutBenutzer(request):
-    logout(request)
-    return redirect('shop')
-
-def regBenutzer(request):
-    seite = 'reg'
-    form = EigeneUserCreationForm
-
-    if request.method == 'POST':
-        form = EigeneUserCreationForm(request.POST)
-        if form.is_valid():
-           benutzer = form.save(commit=False)
-           benutzer.save()
-
-           kunde = Kunde(name=request.POST['username'], benutzer=benutzer)
-           kunde.save()
-           bestellung = Bestellung(kunde=kunde)
-           bestellung.save()
-
-           login(request, benutzer)
-           return redirect('shop')
-        else:
-            messages.error(request, "Fehlerhafte Eingabe!")
-
-    ctx = {'form': form, 'seite': seite}
-    return render(request, 'shop/login.html', ctx)
+def regSeite(request):
+  seite = "reg"
+  form = UserCreationForm
+  
+  if request.method == 'POST':
+    form = UserCreationForm(request.POST)
+    if form.is_valid():
+      benutzer = form.save(commit=False)
+      benutzer.save()
+      
+      kunde = Kunde(name=request.POST['username'], benutzer=benutzer)
+      kunde.save()
+      bestellung = Bestellung(kunde=kunde)
+      bestellung.save()
+      
+      login(request, benutzer)
+      return redirect('shop')
+    else:
+      messages.error(request, "Fehlerhafte Eingabe.")
+  
+  ctx = {'form': form, 'seite':seite}
+  return render(request, 'shop/login.html', ctx)
 
 def bestellen(request):
   auftrags_id = uuid.uuid4()
@@ -148,23 +145,17 @@ def bestellen(request):
   messages.success(request, mark_safe("Danke für Ihre <a href='/bestellung/"+auftragsUrl+"'>Bestellung</a>.</br>Jetzt bezahlen: "+paypalform.render()))
   return JsonResponse('Bestellung erfolgreich', safe=False)
 
-@login_required(login_url='login')
-def bestellung(request,id):
+@login_required(login_url = 'login')
+def bestellung(request, id):
+  bestellung = Bestellung.objects.get(auftrags_id = id)
+  print(bestellung)
+  if bestellung: # and str(request.user) == str(bestellung.kunde):
     bestellung = Bestellung.objects.get(auftrags_id=id)
-
-    if bestellung and str(request.user) == str(bestellung.kunde):
-        bestellung = Bestellung.objects.get(auftrags_id=id)
-        artikels = bestellung.bestellteartikel_set.all()
-        ctx = {'artikels':artikels,'bestellung':bestellung}
-        return render(request, 'shop/bestellung.html',ctx)
-    else:
-        return redirect('shop')
-    
-def fehler404(request, exception):
+    artikels = bestellung.bestellteartikell_set.all()
+    ctx = {'artikels':artikels, 'bestellung':bestellung}
+    return render(request, 'shop/bestellung.html', ctx)
+  else:
     return render(request, 'shop/404.html')
-
-    
-
-
-
-    
+  
+def fehler404(request, exception):
+  return render(request, 'shop/404.html')
