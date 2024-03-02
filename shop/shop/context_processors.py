423a27d3e24d50234e6dfbee832e8db6fa9fe925
Kay <info@beckmann-md.de>
2024-03-02 17:53:36 +0100

    rollback

--- shop/shop/context_processors.py
+++ shop/shop/context_processors.py
@@ -1,24 +1,15 @@
-from . models import Bestellung, BestellteArtikel
-import json
+from . models import Bestellung, BestellteArtikell
 
 def warenkorb_anzahl(request):
-
-    if request.user.is_authenticated:
-        kunde = request.user.kunde
-        bestellung, created = Bestellung.objects.get_or_create(kunde=kunde, erledigt=False)
-
-        if bestellung:
-            menge = bestellung.get_gesamtmenge
-        else:
-            menge = 0
-
+  if request.user.is_authenticated:
+    kunde = request.user.kunde
+    bestellung, created = Bestellung.objects.get_or_create(kunde = kunde, erledigt = False)
+    
+    if bestellung:
+      menge = bestellung.get_gesamtmenge
     else:
-        menge = 0
-        try:
-            warenkorb = json.loads(request.COOKIES['warenkorb'])
-        except:
-            warenkorb = {}
-        for i in warenkorb:
-            menge += warenkorb[i]["menge"]
-
-    return {'menge': menge}
+      menge = 0
+  else:
+    menge = 0
+    
+  return {'menge': menge}
--- shop/static/js/warenkorb.js
+++ shop/static/js/warenkorb.js
@@ -1,90 +1,67 @@
-let bestellenButtons = document.getElementsByClassName("warenkorb-bestellen")
+let bestellenButtons = document.getElementsByClassName("warenkorb-bestellen");
 
-for (let i = 0; i < bestellenButtons.length; i++){
-    bestellenButtons[i].addEventListener('click', function(){
-        let artikelID = this.dataset.artikel;
-        let action = this.dataset.action;
-        
-        if(benutzer == 'AnonymousUser'){
-            updateGastBestellung(artikelID, action)
-        } else{
-            updateKundenBestellung(artikelID, action)
-        }
-    })
+for(let i=0; i < bestellenButtons.length; i++){
+  bestellenButtons[i].addEventListener('click', function() {
+    let artikelId = this.dataset.artikel;
+    let action = this.dataset.action;
+    updateKundenBestellung(artikelId, action)
+  })
 }
 
-function updateGastBestellung(artikelID, action){
-    //console.log("Gast " +artikelID+" "+action)
-    if(action == 'bestellen'){
-        if(warenkorb[artikelID] == undefined){
-            warenkorb[artikelID] = {'menge':1}
-        } else{
-            warenkorb[artikelID]['menge'] +=1
-        }
-    }
-    if(action == 'entfernen'){
-        warenkorb[artikelID]['menge'] -=1
+function updateKundenBestellung(artikelId, action){
+  let url = "/artikel_backend/"
 
-        if(warenkorb[artikelID]['menge'] <=0){
-            delete warenkorb[artikelID]
-        }
-    }
-    document.cookie = 'warenkorb=' + JSON.stringify(warenkorb) + ";domain;path=/; SameSite=None; Secure"
-    console.log(warenkorb)
+  fetch(url,{
+    method:'post',
+    headers:{
+      'Content-Type':'application/json',
+      'X-CSRFToken':csrftoken,
+    },
+    body:JSON.stringify({'artikelId':artikelId, 'action':action})
+  })
+  .then(()=>{
     location.reload()
+  })
 }
 
-function updateKundenBestellung(artikelID, action){
-    let url = "/artikel_backend/";
-
-    fetch(url, {
-        method: 'post',
-        headers:{
-            'Content-Type':'application/json',
-            'X-CSRFToken':csrftoken,
-        },
-        body:JSON.stringify({'artikelID':artikelID, 'action':action})
-    })
-    .then(()=>location.reload())
-}
-
-//Kasse
+//KASSE
 let formular = document.getElementById('formular')
 let gesamtpreis = document.getElementById('gesamtpreis').value
 
 formular.addEventListener('submit', function(e){
-    e.preventDefault()
-    document.getElementById('formular-button').classList.add('d-none');
-    document.getElementById('bezahlen-info').classList.remove('d-none');
+  e.preventDefault()
+  document.getElementById('formular-button').classList.add('d-none')
+  document.getElementById('bezahlen-info').classList.remove('d-none')
 })
 
 document.getElementById('bezahlen-button').addEventListener('click', function(e){
-    submitFormular()
+  submitFormular();
 })
 
 function submitFormular(){
-    let benutzerDaten = {
-        'name': formular.inputName.value,
-        'email': formular.inputEmail.value,
-        'gesamtpreis': gesamtpreis
-    }
-    let lieferadresse = {
-        'adresse': formular.inputAdresse.value,
-        'plz': formular.inputPlz.value,
-        'stadt': formular.inputStadt.value,
-        'land': formular.inputLand.value,
-    }
-    console.log(benutzerDaten, lieferadresse)
-    
-    let url = "/bestellen/";
+  let benutzerdaten = {
+    'name': formular.inputName.value,
+    'email': formular.inputEmail.value,
+    'gesamtpreis': gesamtpreis
+  }
+  let lieferadresse = {
+    'adresse':formular.inputAdresse.value,
+    'plz':formular.inputPlz.value,
+    'stadt':formular.inputStadt.value,
+    'land':formular.inputLand.value,
+  }
 
-    fetch(url, {
-        method: 'post',
-        headers:{
-            'Content-Type':'application/json',
-            'X-CSRFToken':csrftoken,
-        },
-        body:JSON.stringify({'benutzerDaten':benutzerDaten, 'lieferadresse':lieferadresse})
-    })
-    .then(()=>window.location.href="/")
+  let url = "/bestellen/"
+  
+  fetch(url,{
+    method:'post',
+    headers:{
+      'Content-Type':'application/json',
+      'X-CSRFToken':csrftoken,
+    },
+    body:JSON.stringify({'benutzerdaten':benutzerdaten, 'lieferadresse':lieferadresse})
+  })
+  .then(()=>{
+    window.location.href="/"
+  })
 }
